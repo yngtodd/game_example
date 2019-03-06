21698be55bf38c9d124b44bfaca30578a33debc2
yngtodd <young.todd.mk@gmail.com>
2019-03-05 21:20:03 -0500

    Ready to Rock

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -6,7 +6,9 @@ namespace engine {
 
   void print_message()
   {
+      torch::Tensor tensor = torch::eye(3);
       std::cout << "Hello World!" << std::endl;
+      std::cout << "Engine eye:\n" << tensor << std::endl;
   }
 
 }
